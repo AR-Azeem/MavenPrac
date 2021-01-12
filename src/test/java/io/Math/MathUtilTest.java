a8b606304123c454b7f0e974b5cbf02787e4582a
ABDUL AZEEM <abdul.azeem@daimler.com>
2021-01-12 17:06:51 +0530

    Added one more test

--- src/test/java/io/Math/MathUtilTest.java
+++ src/test/java/io/Math/MathUtilTest.java
@@ -2,16 +2,56 @@ package io.Math;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class MathUtilTest {
-
+	MathUtil mathutil;
+	static int c =0;
+	{
+		c+=1;
+		System.out.println(c);
+	}
+	@BeforeAll
+	static void befroreall() {
+		System.out.println("hello");
+	}
+	
+	
+	@BeforeEach
+	void inti() {
+		mathutil = new MathUtil();
+	}
+	
 	@Test
 	void test() {
-		MathUtil mathutil = new MathUtil();
-		int expected = 3;
+		//MathUtil mathutil = new MathUtil();
+		int expected = 2;
 		int actual = mathutil.add(1,1);
 		assertEquals(expected,actual);
 	}
+	@Test
+	void testDivide() {
+		
+		assertThrows(ArithmeticException.class,()->mathutil.divide(1,0));
+		System.out.println("Hello maven Jenkins");
+	}
+	
+	@Test
+	void testDivide1() {
+		
+		assertThrows(ArithmeticException.class,()->mathutil.divide(4,0));
+	}
+	
+	@Test
+	void testDivide2() {
+		
+		assertThrows(ArithmeticException.class,()->mathutil.divide(4,0));
+	}
+	
+	
 
 }
